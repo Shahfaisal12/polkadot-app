c192b542475e4691cc50906b48bc294720c0eb89
shahfaisal12 <faisalmkd02@gmail.com>
2022-10-22 15:47:23 +0500

    rotate

--- src/components/elements/blog/ToGetStart.jsx
+++ src/components/elements/blog/ToGetStart.jsx
@@ -7,7 +7,7 @@ const ToGetStart = () => {
     <div class="row justify-content-center pb-0 pb-md-5">
       <div class="col col-6 text-center">
         <h1 class="mb-4">Subscribe to the newsletter to hear about updates and events.</h1>
-        <a class="btn btn-subscribe btn-lg color-E6007" href="/">Build on Polkadot</a>
+        <a class="btn btn-subscribe btn-lg color-E6007A" href="/">Build on Polkadot</a>
       </div>
     </div>
   </div>
